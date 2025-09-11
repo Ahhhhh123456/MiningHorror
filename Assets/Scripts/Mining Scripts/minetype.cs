7d8f152437b5f02ceb89d795bffc00f29b4abbae
Jason Li <Jason.Li8885551@gmail.com>
2025-09-10 20:11:57 -0700

    Got particles to work
    Might need to adjust them later on. Currently, it shoots and ends with particles

--- Assets/Scripts/Mining Scripts/minetype.cs
+++ Assets/Scripts/Mining Scripts/minetype.cs
@@ -13,39 +13,66 @@ public class MineType : MonoBehaviour
     public int holdCount = 0;
 
     [Header("Effects")]
-    public ParticleSystem breakParticles; 
+    public ParticleSystem ParticleFullyBreak;
+
+    public ParticleSystem ParticleBreaking; 
 
 
     public void Mining()
     {
         string tag = gameObject.tag;
         holdCount += 1;
-                
+
         Debug.Log("Count:" + holdCount);
         if (rockTypes.ContainsKey(tag))
         {
             RockData data = rockTypes[tag];
 
-            if (holdCount == data.durability)
+            // Particles midway through mining. Keep for later use maybe.
+            // if (tag != "Stone" && holdCount == data.durability / 2)
+            // {
+            //     if (ParticleBreaking != null)
+            //     {
+            //         ParticleSystem PB = Instantiate(ParticleBreaking, transform.position, Quaternion.identity);
+            //         PB.Play();
+            //         Destroy(PB.gameObject, PB.main.duration + PB.main.startLifetime.constantMax);
+            //     }
+            // }
+
+            // Particles on first hit of object
+            if (holdCount == 1)
             {
-                if (breakParticles != null)
+                if (ParticleBreaking != null)
                 {
-                    Debug.Log("Playing break particles");
-                    breakParticles.Play();
+                    ParticleSystem PB = Instantiate(ParticleBreaking, transform.position, Quaternion.identity);
+                    PB.Play();
+                    Destroy(PB.gameObject, PB.main.duration + PB.main.startLifetime.constantMax);
                 }
-
-                Dropped dropscript = GetComponent<Dropped>();
-                if (dropscript != null)
-                {
-                    dropscript.DropItem(gameObject);
-                }
-
-
-                //Debug.Log("Mining complete for: " + tag);
-                gameObject.SetActive(false);
-
-                holdCount = 0;
             }
+
+            if (holdCount == data.durability)
+                {
+
+                    if (ParticleFullyBreak != null)
+                    {
+                        ParticleSystem PFB = Instantiate(ParticleFullyBreak, transform.position, Quaternion.identity);
+                        PFB.Play();
+                        Destroy(PFB.gameObject, PFB.main.duration + PFB.main.startLifetime.constantMax);
+
+                    }
+
+                    Dropped dropscript = GetComponent<Dropped>();
+                    if (dropscript != null)
+                    {
+                        dropscript.DropItem(gameObject);
+                    }
+
+
+                    //Debug.Log("Mining complete for: " + tag);
+                    gameObject.SetActive(false);
+
+                    holdCount = 0;
+                }
         }
         else
         {

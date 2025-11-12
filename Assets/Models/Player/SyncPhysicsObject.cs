d48b4eeb150863b2f48fa1853b7941f8a53b5ab4
edmfong <150398230+edweeniee@users.noreply.github.com>
2025-11-11 18:46:29 -0800

    added active ragdoll movement

---
+++ Assets/Models/Player/SyncPhysicsObject.cs
@@ -0,0 +1,32 @@
+using JetBrains.Rider.Unity.Editor;
+using UnityEngine;
+
+public class SyncPhysicsObject : MonoBehaviour
+{
+    Rigidbody rigidbody3D;
+    ConfigurableJoint joint;
+
+    [SerializeField] Rigidbody animatedRigidbody3D;
+
+    [SerializeField] bool syncAnimation = false;
+
+    // Keep track for starting rotation
+    Quaternion startLocalRotation;
+
+    void Awake()
+    {
+        rigidbody3D = GetComponent<Rigidbody>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        // Store the starting local rotation
+        startLocalRotation = transform.localRotation;
+    }
+
+    public void UpdateJointFromAnimation()
+    {
+        if (!syncAnimation)
+            return;
+
+        ConfigurableJointExtensions.SetTargetRotationLocal(joint, animatedRigidbody3D.transform.localRotation, startLocalRotation);
+    }
+}
